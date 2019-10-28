76bc497297be134dd95d256be1f5b9fab12d8a54
Vladislav Panfilov <andromalak222@gmail.com>
2019-10-29 00:26:35 +0500

    8

---
+++ app/Project.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+
+class Project extends Model
+{
+    protected $guarded = [];
+
+    public function path()
+    {
+        return "/projects/{$this->id}";
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
