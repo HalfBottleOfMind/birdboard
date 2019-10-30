249d27a5bdaef1f74f7b1fb4c83bcf044d993037
Vladislav Panfilov <andromalak222@gmail.com>
2019-10-30 09:47:02 +0500

    10

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,14 +1,22 @@
 @extends ('layouts.app')
 
 @section('content')
-	<div class="flex items-center mb-3">
-		<a href="/projects/create">New Project</a>
-	</div>
-	<ul>
+	<header class="flex items-center mb-3 py-4">
+		<div class="flex justify-between items-center w-full">
+			<h2 class="text-gray-600 text-sm">My Projects</h2>
+			<a href="/projects/create" class="button">New Project</a>
+		</div>
+	</header>
+	<main class="lg:flex lg:flex-wrap -mx-3">
 		@forelse ($projects as $project)
-			<li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
+			<div class="lg:w-1/3 px-3 pb-6">
+				<div class="bg-white rounded-lg shadow p-5" style="height: 200px">
+					<h3 class="font-normal text-xl py-4 pl-4 -ml-5 mb-3 border-l-4 border-blue-600"><a href="{{ $project->path() }}">{{ $project->title }}</a></h3>
+					<div class="text-gray-600">{{ Str::limit($project->description, 100) }}</div>
+				</div>
+			</div>
 		@empty
-			<li>No projects</li>
+			<div>No Projects yet</div>
 		@endforelse
-	</ul>
+	</main>
 @endsection
