fe4a85e57539a3878abf8019af32b871e679f280
Vladislav Panfilov <andromalak222@gmail.com>
2019-10-31 01:16:32 +0500

    11

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,35 @@
 @extends ('layouts.app')
 
 @section('content')
-	<h1>{{ $project->title }}</h1>
-	<div>{{ $project->description }}</div>
-	<a href="/projects">Go Back</a>
+	<header class="flex items-center mb-3 py-4">
+		<div class="flex justify-between items-end w-full">
+				<p class="text-gray-600 text-sm">
+					<a href="/projects">My Projects</a> / {{ $project->title }}
+				</p>
+			<a href="/projects/create" class="button">New Project</a>
+		</div>
+	</header>
+
+	<main>
+		<div class="lg:flex -mx-3">
+			<div class="lg:w-3/4 px-3 mb-6">
+				<div class="mb-6">
+					<div><h2 class="text-gray-600 text-lg mb-3">Tasks</h2></div>
+					
+					<div class="card mb-3">Lorem ipsum.</div>
+					<div class="card mb-3">Lorem ipsum.</div>
+					<div class="card mb-3">Lorem ipsum.</div>
+					<div class="card">Lorem ipsum.</div>
+				</div>
+				<div>
+					<div><h2 class="text-gray-600 text-lg mb-3">General Notes</h2></div>
+
+					<textarea class="card w-full" style="min-height: 200px">Lorem ipsum.</textarea>
+				</div>
+			</div>
+			<div class="lg:w-1/4 px-3">
+				@include('projects.card')
+			</div>
+		</div>
+	</main>
 @endsection
