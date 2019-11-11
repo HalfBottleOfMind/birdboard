48e9b276eea1bc2dc6cbb7f003cd654f0f0fc311
Vladislav Panfilov <andromalak222@gmail.com>
2019-11-11 11:49:01 +0500

    31

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -140,4 +140,12 @@ class ManageProjectsTest extends TestCase
 		$project = factory('App\Project')->create();
 		$this->delete($project->path(), [])->assertStatus(403);
 	}
+
+	/** @test */
+	public function a_user_can_see_all_projects_they_have_been_invited_to_on_their_dashboard()
+	{
+		$project = tap(ProjectFactory::create())->invite($this->signIn());
+
+		$this->get('/projects')->assertSee($project->title);
+	}
 }
