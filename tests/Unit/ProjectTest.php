4fde5decf9d076befd787d2a009214808d750846
Vladislav Panfilov <andromalak222@gmail.com>
2019-11-11 11:13:30 +0500

    30

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -33,5 +34,15 @@ class ProjectTest extends TestCase
 		$task = $project->addTask('Test task');
 		$this->assertCount(1, $project->tasks);
 		$this->assertTrue($project->tasks->contains($task));
-	}
+    }
+    
+    /** @test */
+    public function it_can_invite_a_user()
+    {
+        $project = factory('App\Project')->create();
+
+        $project->invite($user = factory(User::class)->create());
+
+        $this->assertTrue($project->members->contains($user));
+    }
 }
